c757c7e21580abc4c0ee55cf51d2ec54504ca365
mdmkl6 <michdom1lo@gmail.com>
2024-01-26 19:22:23 +0100

    fix azure function

--- Azure_Functions_Sentiment/function_app.py
+++ Azure_Functions_Sentiment/function_app.py
@@ -1,13 +1,14 @@
-import logging
-
 import azure.functions as func
+import logging
+import json
 from pattern.text.en import sentiment
 
+app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
 
-def main(req: func.HttpRequest) -> func.HttpResponse:
+@app.route(route="Azure_Functions_Sentiment")
+def Azure_Functions_Sentiment(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    # Parse the input text
     text = req.params.get('text')
     if not text:
         try:
@@ -18,17 +19,22 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             text = req_body.get('text')
 
     if text:
-        # Analyze sentiment
         sentiment_value = sentiment(text)
         logging.info(f"Sentiment analysis result: {sentiment_value}")
 
-        # Return the sentiment value
+        sentiment_response = {
+            "polarity": sentiment_value[0],
+            "subjectivity": sentiment_value[1]
+        }
+
         return func.HttpResponse(
-            f"The sentiment of the text is: {sentiment_value}",
-            status_code=200
+            json.dumps(sentiment_response),
+            status_code=200,
+            headers={"Content-Type": "application/json"}
         )
     else:
         return func.HttpResponse(
             "Please pass a text on the query string or in the request body",
             status_code=400
         )
+
